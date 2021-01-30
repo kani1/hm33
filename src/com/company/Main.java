39764edf64425c2611f728496198e110115301e6
kani1 <kazakovakanykey@gmail.com>
2021-01-30 18:30:21 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int [] array= new int[] {6,2,4,5,3,8,9,1};
+        Arrays.sort(array);
+        System.out.println(Arrays.toString(array));
+
+
+    }
+}
